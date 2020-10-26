c358e1d165f605689030189b038c228a16c86cd0
Gianluca De Cola <gianluca.decola@ags-it.com>
2020-10-26 15:23:33 +0100

    working project starter.

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ echo drf-yasg==$3 >> src/requirements.txt
 
 docker build -t django-project:latest src/
 docker create -ti --name dummy django-project:latest bash
-docker cp dummy:/src/* $(pwd)/src/
+docker cp dummy:/src/manage.py $(pwd)/src/
+docker cp dummy:/src/server $(pwd)/src/
 docker rm -f dummy
