f8a0cba1e2b5a43f085d2993728051b7b339cf3e
Gianluca De Cola <gianluca.decola@ags-it.com>
2020-10-27 11:41:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,13 @@ echo django==$1 > src/requirements.txt
 echo djangorestframework==$2 >> src/requirements.txt
 echo drf-yasg==$3 >> src/requirements.txt
 
-docker build -t django-project:builder src/
+docker build --build-arg appname=$4 -t django-project:builder src/
 docker create -ti --name dummy django-project:builder bash
+
 docker cp dummy:/src/manage.py $(pwd)/src/
 docker cp dummy:/src/server $(pwd)/src/
+docker cp dummy:/src/$4 $(pwd)/src/
+
 docker rm -f dummy
 docker rmi django-project:builder
 
